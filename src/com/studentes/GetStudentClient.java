11ed5b9555ba9c620d36243146cb48e95e94c7d9
s7263 <s7263@hlopezmw7.grupoib.local>
2013-04-08 04:24:05 -0500

    HERNAN: Subiendo el proyecto RestFul WebService Client de Students.

---
+++ src/com/studentes/GetStudentClient.java
@@ -0,0 +1,126 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.studentes;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.UniformInterfaceException;
+import com.sun.jersey.api.client.WebResource;
+import java.util.ArrayList;
+import java.util.List;
+import com.studentes.Students;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.GenericType;
+
+/**
+ * Jersey REST client generated for REST resource:StudentsFacadeREST
+ * [com.students.students]<br>
+ * USAGE:
+ * <pre>
+ *        NewJerseyClient client = new NewJerseyClient();
+ *        Object response = client.XXX(...);
+ *        // do whatever with response
+ *        client.close();
+ * </pre>
+ *
+ * @author s7263
+ */
+public class GetStudentClient {
+    private WebResource webResource;
+    private Client client;
+    private static final String BASE_URI = "http://localhost:8080/dsdpc2ServerApp/webresources";
+
+    public GetStudentClient() {
+        com.sun.jersey.api.client.config.ClientConfig config = new com.sun.jersey.api.client.config.DefaultClientConfig();
+        client = Client.create(config);
+        webResource = client.resource(BASE_URI).path("com.students.students");
+    }
+
+    public void remove(String id) throws UniformInterfaceException {
+        webResource.path(java.text.MessageFormat.format("{0}", new Object[]{id})).delete();
+    }
+
+    public String countREST() throws UniformInterfaceException {
+        WebResource resource = webResource;
+        resource = resource.path("count");
+        return resource.accept(javax.ws.rs.core.MediaType.TEXT_PLAIN).get(String.class);
+    }
+
+    public <T> T findAll_XML(Class<T> responseType) throws UniformInterfaceException {
+        WebResource resource = webResource;
+        return resource.accept(javax.ws.rs.core.MediaType.APPLICATION_XML).get(responseType);
+    }
+
+    public <T> T findAll_JSON(Class<T> responseType) throws UniformInterfaceException {
+        WebResource resource = webResource;
+        return resource.accept(javax.ws.rs.core.MediaType.APPLICATION_JSON).get(responseType);
+    }
+
+    public void edit_XML(Object requestEntity) throws UniformInterfaceException {
+        webResource.type(javax.ws.rs.core.MediaType.APPLICATION_XML).put(requestEntity);
+    }
+
+    public void edit_JSON(Object requestEntity) throws UniformInterfaceException {
+        webResource.type(javax.ws.rs.core.MediaType.APPLICATION_JSON).put(requestEntity);
+    }
+
+    public void create_XML(Object requestEntity) throws UniformInterfaceException {
+        webResource.type(javax.ws.rs.core.MediaType.APPLICATION_XML).post(requestEntity);
+    }
+
+    public void create_JSON(Object requestEntity) throws UniformInterfaceException {
+        webResource.type(javax.ws.rs.core.MediaType.APPLICATION_JSON).post(requestEntity);
+    }
+
+    public <T> T findRange_XML(Class<T> responseType, String from, String to) throws UniformInterfaceException {
+        WebResource resource = webResource;
+        resource = resource.path(java.text.MessageFormat.format("{0}/{1}", new Object[]{from, to}));
+        return resource.accept(javax.ws.rs.core.MediaType.APPLICATION_XML).get(responseType);
+    }
+
+    public <T> T findRange_JSON(Class<T> responseType, String from, String to) throws UniformInterfaceException {
+        WebResource resource = webResource;
+        resource = resource.path(java.text.MessageFormat.format("{0}/{1}", new Object[]{from, to}));
+        return resource.accept(javax.ws.rs.core.MediaType.APPLICATION_JSON).get(responseType);
+    }
+
+    public <T> T find_XML(Class<T> responseType, String id) throws UniformInterfaceException {
+        WebResource resource = webResource;
+        resource = resource.path(java.text.MessageFormat.format("{0}", new Object[]{id}));
+        return resource.accept(javax.ws.rs.core.MediaType.APPLICATION_XML).get(responseType);
+    }
+
+    public <T> T find_JSON(Class<T> responseType, String id) throws UniformInterfaceException {
+        WebResource resource = webResource;
+        resource = resource.path(java.text.MessageFormat.format("{0}", new Object[]{id}));
+        return resource.accept(javax.ws.rs.core.MediaType.APPLICATION_JSON).get(responseType);
+    }
+
+    public void close() {
+        client.destroy();
+    }
+    
+    public static void main(String args[])throws UniformInterfaceException {
+
+        GetStudentClient client1=new GetStudentClient();
+        ClientResponse response=client1.findAll_XML(ClientResponse.class);
+
+
+        GenericType<List<Students>> genericType = new GenericType<List<Students>>() {};
+        
+        List<Students> data= new ArrayList<>();
+        data=(response.getEntity(genericType));
+        System.out.println("Listado de Students. ");
+        
+        for( Students studentss:data ){
+            System.out.println("Idbooster: "+studentss.getIdbooster());
+            System.out.println("Firstname: "+studentss.getFirstname());
+            System.out.println("Lastname:  "+studentss.getLastname());
+            System.out.println("Birthday:  "+studentss.getBirthday());
+            System.out.println("-----------");
+        }
+
+    }
+
+}
